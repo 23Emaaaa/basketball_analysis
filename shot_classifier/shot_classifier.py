77c301b623f320099bc94bfb63648cd7bc6c49f1
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2025-07-30 18:53:08 +0200

    Refactor ShotClassifier to implement shot classification logic based on player position; differentiate between 2-pointer and 3-pointer shots.

--- shot_classifier/shot_classifier.py
+++ shot_classifier/shot_classifier.py
@@ -1,7 +1,18 @@
 class ShotClassifier:
     def __init__(self, court_dimensions):
         self.court_dimensions = court_dimensions
+        # Placeholder for the 3-point line x-coordinate
+        self.three_point_line_x = (
+            250  # This should be calculated based on court_dimensions
+        )
 
     def classify(self, shot_event, player_position):
-        # For now, we'll just classify all shots as 2-point shots
-        return {"shot_type": "2-pointer"}
+        # player_position is a bbox [x1, y1, x2, y2]
+        # We take the bottom center of the bbox as the player's position
+        x = (player_position[0] + player_position[2]) / 2
+
+        shot_type = "2-pointer"
+        if x < self.three_point_line_x:
+            shot_type = "3-pointer"
+
+        return {"shot_type": shot_type}
