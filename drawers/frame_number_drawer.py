e9be2fc52bcdab08862dadd39e51b44201b40ea9
Emanuele Dajko <emanuele.dajko@studio.unibo.it>
2025-07-30 18:54:04 +0200

    Refactor FrameNumberDrawer's draw method for improved readability; adjust formatting and spacing in code.

--- drawers/frame_number_drawer.py
+++ drawers/frame_number_drawer.py
@@ -1,13 +1,17 @@
 import cv2
+
+
 class FrameNumberDrawer:
     def __init__(self):
         pass
 
-    def draw(self,frames):
+    def draw(self, frames):
         # Write the frame number on the top left corner of the frame
         output_frames = []
         for i in range(len(frames)):
             frame = frames[i].copy()
-            cv2.putText(frame, str(i), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(
+                frame, str(i), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2
+            )
             output_frames.append(frame)
         return output_frames
